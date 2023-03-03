6d49dfcee3569132485b56d2ec0b94bd72477889
Sheela Khusal <sheela.khusal.08@gmail.com>
2023-03-03 13:40:22 -0300

    refactor: page form
    Co-authored-by: 0xEDU <bfwaend@gmail.com>
    Co-authored-by: wleite <av4t@hotmail.com>

--- front/my_app/lib/pages/page_form.dart
+++ front/my_app/lib/pages/page_form.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
 
+import '../widgets/widget_appbar_form.dart';
+import '../widgets/widget_body_form.dart';
+import '../widgets/widget_bottom_navigation_bar_form.dart';
+import '../widgets/widget_bottom_navigation_bar_form.dart';
+
 class FormPage extends StatefulWidget {
   FormPage({super.key, required this.title});
 
@@ -10,98 +15,13 @@ class FormPage extends StatefulWidget {
 }
 
 class _FormPageState extends State<FormPage> {
-  final Map<String, bool> skillsList = {
-    "Agilidade": false,
-    "Front-end Development": false,
-    "Back-end Development": true,
-    "Ux/Ui Design": false,
-    "Graphic Design": false,
-    "Lógica de Programação": false,
-    "Lorem Ipsum1": false,
-    "Lorem Ipsum2": false,
-    "Lorem Ipsum3": false,
-    "Lorem Ipsum4": false
-  };
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return const Scaffold(
       extendBodyBehindAppBar: true,
-      appBar: AppBar(
-        title: Text(
-          "Adicionar Skills",
-          style: TextStyle(
-            color: Colors.white,
-          ),
-        ),
-        backgroundColor: Colors.transparent,
-        elevation: 0,
-      ),
-      body: Container(
-        padding: EdgeInsets.only(top: 50),
-        decoration: const BoxDecoration(
-          gradient: LinearGradient(
-            begin: Alignment.topCenter,
-            end: Alignment.bottomCenter,
-            colors: [
-              Color.fromARGB(204, 10, 125, 184),
-              Color.fromARGB(204, 0, 40, 60)
-            ],
-          ),
-        ),
-        width: double.infinity,
-        child: Container(
-          padding: EdgeInsets.fromLTRB(10, 0, 10, 10),
-          color: Color(0xFFEBF0FF),
-          child: ListView.builder(
-            itemCount: skillsList.length,
-            itemBuilder: (context, index) {
-              String key = skillsList.keys.elementAt(index);
-              bool isSelected = skillsList[key]!;
-              return Column(
-                children: [
-                  ListTile(
-                    onTap: () {
-                      skillsList[key] = !skillsList[key]!;
-                      isSelected = skillsList[key]!;
-                      setState(() {});
-                    },
-                    title: Text(
-                      skillsList.keys.elementAt(index),
-                      style: TextStyle(
-                        color: isSelected
-                            ? const Color(0xFFD9385E)
-                            : const Color(0xFF535353),
-                        fontWeight:
-                            isSelected ? FontWeight.bold : FontWeight.normal,
-                        fontSize: 15,
-                      ),
-                    ),
-                  ),
-                  Divider(
-                    thickness: 1,
-                    color: Color(0xFFAFAFAF),
-                  ),
-                ],
-              );
-            },
-          ),
-        ),
-      ),
-      bottomNavigationBar: Container(
-        color: Color(0xFFEBF0FF),
-        padding: EdgeInsets.symmetric(horizontal: 15, vertical: 25),
-        child: ElevatedButton(
-          onPressed: () {},
-          style: ElevatedButton.styleFrom(
-            minimumSize: const Size.fromHeight(50),
-          ),
-          child: const Text(
-            "Salvar",
-            style: TextStyle(fontSize: 20),
-          ),
-        ),
-      ),
+      appBar: AppBarFormWidget(title: "Adicionar Skills"),
+      body: BodyFormWidget(),
+      bottomNavigationBar: BottomNavigationBarFormWidget(),
     );
   }
 }
