179f6ef859e9d70a6b0d0d37a91c5cf1e8d7c27d
José Araújo <jose.filho@tqi.com.br>
2023-03-07 11:38:13 -0300

    Script inicial de banco de dados

--- java/mentorme/src/main/resources/db/migration/V0__initial.sql
+++ java/mentorme/src/main/resources/db/migration/V0__initial.sql
@@ -5,3 +5,29 @@ CREATE TABLE person (
    can_teach BOOLEAN,
    want_to_learn BOOLEAN
 );
+
+CREATE TABLE contact_type (
+   id bigint PRIMARY KEY,
+   type VARCHAR (10)
+);
+
+CREATE TABLE contact (
+   id bigint PRIMARY KEY,
+   id_person bigint NOT NULL,
+   id_contact_type bigint NOT NULL,
+   contact_value VARCHAR(150),
+   CONSTRAINT fk_contact_person FOREIGN KEY(id_person) REFERENCES person(id),
+   CONSTRAINT fk_contact_type FOREIGN KEY(id_contact_type) REFERENCES contact_type(id)
+);
+
+CREATE TABLE contact_history(
+   id bigint PRIMARY KEY,
+   id_person_to bigint NOT NULL,
+   id_person_from bigint NOT NULL,
+   contact_type VARCHAR(10),
+   contact_value VARCHAR(150),
+   created TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+   request_type VARCHAR(50),
+   CONSTRAINT fk_hist_contact_person_to FOREIGN KEY(id_person_to) REFERENCES person(id),
+   CONSTRAINT fk_hist_contact_person_from FOREIGN KEY(id_person_to) REFERENCES person(id)
+);
