eb2b7332cc0a107d3331cdd31a7c358c0ce0f29a
Salger Luiz de Oliveira <salger.oliveira@tqi.com.br>
2023-03-07 15:14:32 -0300

    ajustes no personresponse

--- java/mentorme/src/main/java/br/com/agile/communit/mentorme/response/ContactResponse.java
+++ java/mentorme/src/main/java/br/com/agile/communit/mentorme/response/ContactResponse.java
@@ -2,23 +2,16 @@ package br.com.agile.communit.mentorme.response;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
+
 import lombok.Data;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 @Data
-public class PersonResponse {
-    @JsonProperty("image")
-    private byte[] image;
+public class ContactResponse {
     @JsonProperty("id")
     private Integer id;
     @JsonProperty("name")
     private String name;
     @JsonProperty("description")
     private String description;
-    @JsonProperty("can_teach")
-    private boolean canTeach;
-    @JsonProperty("want_to_learn")
-    private boolean wantToLearn;
-    @JsonProperty("phoneNumber")
-    private boolean phoneNumber;
 }
