d1c88fb94a618252770bd788a712984bfdfb31bc
José Araújo <jose.filho@tqi.com.br>
2023-03-07 10:17:23 -0300

    Iniciando projeto Java

---
+++ java/mertorme/src/main/java/br/com/agile/communit/mentorme/MertormeApplication.java
@@ -0,0 +1,13 @@
+package br.com.agile.communit.mentorme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MertormeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MertormeApplication.class, args);
+	}
+
+}
---
+++ java/mertorme/src/test/java/br/com/agile/communit/mentorme/MertormeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.agile.communit.mentorme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MertormeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
