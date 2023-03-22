41a0b7c4d2f8f82c7ad3e57ff07bf9a2f6f2f749
wleite <wleite@student.42sp.org.br>
2023-03-22 12:12:46 -0300

    chore: add dev containers application profile
    Co-authored-by: sgkhusal <sheela.khusal.08@gmail.com>
    Co-authored-by: 0xEDU <bfwaend@gmail.com>

--- java/script.sh
+++ java/script.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 ./gradlew clean build -x test
-java -jar -Dspring.profiles.active=local ./build/libs/mentorme-0.0.1-SNAPSHOT.jar
+java -jar -Dspring.profiles.active=dev-containers ./build/libs/mentorme-0.0.1-SNAPSHOT.jar
